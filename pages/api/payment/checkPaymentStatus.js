7c58278a41e5b7d2c130e90d5f93ab86505286ff
dimasgusti <dimsgusticode@gmail.com>
2025-01-16 00:13:24 +0800

    refactor: checkPaymentStatus API

--- pages/api/payment/checkPaymentStatus.js
+++ pages/api/payment/checkPaymentStatus.js
@@ -9,34 +9,21 @@ export default async function handler(req, res) {
     }
 
     try {
-      console.log(`Checking payment status for transaction: ${transactionId}`);
-
       const paymentStatusResponse = await axios.get(`https://api.mayar.id/hl/v1/payment/${transactionId}`, {
         headers: {
           'Authorization': `Bearer ${process.env.MAYAR_API_KEY}`,
         },
       });
-      
-      console.log('Payment status response:', paymentStatusResponse.data);
-      
+
       if (paymentStatusResponse.data.statusCode === 200) {
         const status = paymentStatusResponse.data.data.status;
 
-        if (status === 'active') {
-          res.status(200).json({ success: true, status: 'Payment is active' });
-        } else if (status === 'closed') {
-          res.status(200).json({ success: true, status: 'Payment is completed' });
-        } else if (status === 'unlisted') {
-          res.status(200).json({ success: true, status: 'Payment is unlisted' });
-        } else {
-          console.error('Unknown status from Mayar:', status);
-          res.status(400).json({ success: false, error: 'Unknown payment status from Mayar API' });
-        }
+        return res.status(200).json({ success: true, status });
       } else {
-        res.status(400).json({ success: false, error: 'Payment not found' });
+        return res.status(400).json({ success: false, error: 'Payment not found or invalid status' });
       }
     } catch (error) {
-      console.error('Error checking payment status:', error);
+      console.error('Error fetching payment status:', error);
       return res.status(500).json({ success: false, error: 'Internal Server Error' });
     }
   } else {
