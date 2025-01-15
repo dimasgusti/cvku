b4291831035f511a5556beca179bd3140cd1e271
dimasgusti <dimsgusticode@gmail.com>
2025-01-15 21:57:06 +0800

    test: Mayar POST webhook

---
+++ pages/api/webhook/mayar.js
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+
+export async function POST(request) {
+  try {
+    const body = await request.json();
+    console.log('Data webhook diterima:', body);
+
+    if (body.status === 'success') {
+      console.log(`Pembayaran berhasil untuk transaksi ${body.transactionId}`);
+    }
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error('Error dalam menangani webhook:', error);
+    return NextResponse.json({ success: false, error: error.message });
+  }
+}
