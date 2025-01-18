c09e4475f074d12b85cba10cbe3233521b27275b
dimasgusti <dimsgusticode@gmail.com>
2025-01-18 17:34:57 +0800

    test: removing anything

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,36 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
+// import { SessionProvider } from "next-auth/react";
+// import { Session } from "next-auth";
 import "./globals.css";
-import { Toaster } from "sonner";
-import { Manrope } from "next/font/google";
+// import { Toaster } from "sonner";
+// import { Manrope } from "next/font/google";
 // import Navbar from "@/components/Navbar";
 
-const manrope = Manrope({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
+// const manrope = Manrope({
+//   weight: "400",
+//   subsets: ["latin"],
+//   display: "swap",
+// });
 
 export default function RootLayout({
   children,
-  session,
+  // session,
 }: {
   children: React.ReactNode; 
-  session: Session | null; 
-}): JSX.Element { 
+  // session: Session | null; 
+}) { 
   return (
     <html lang="en">
-      <body className={manrope.className}>
-        <SessionProvider session={session}>
+      {/* <body className={manrope.className}> */}
+      <body>
+        {/* <SessionProvider session={session}> */}
           <main className="text-sm antialiased flex flex-col min-h-screen w-full bg-white">
             {/* <Navbar /> */}
             {children}
-            <Toaster />
+            {/* <Toaster /> */}
           </main>
-        </SessionProvider>
+        {/* </SessionProvider> */}
       </body>
     </html>
   );
