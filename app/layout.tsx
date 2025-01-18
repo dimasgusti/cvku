6e30e65dc7514ab9013400449fb9d83f61b21104
dimasgusti <dimsgusticode@gmail.com>
2025-01-18 16:03:02 +0800

    fix: layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,27 +25,10 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={meta.description} />
-        <meta name="keywords" content={meta.keywords} />
-        <meta name="robots" content={meta.robots} />
-        <link rel="canonical" href={meta.canonical} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:image" content={meta.image} />
-        <meta property="og:url" content={meta.canonical} />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <title>{meta.title}</title>
-      </head>
       <body className={manrope.className}>
         <SessionProvider session={session}>
           <main className="text-sm antialiased flex flex-col min-h-screen w-full bg-white">
-            {!session ? <Navbar /> : null}
+            <Navbar />
             {children}
             <Toaster />
           </main>
