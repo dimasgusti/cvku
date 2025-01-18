6413514d0daccb91a18edc44a3043b322d5fabc4
dimasgusti <dimsgusticode@gmail.com>
2025-01-18 18:51:23 +0800

    feat: responsive navbar

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -6,31 +6,31 @@ import { signIn } from "next-auth/react";
 export default function Pricing() {
   return (
     <div className="flex flex-row justify-center items-center">
-      <div className="w-full sm:w-[360px] md:w-[420px] lg:w-[640px] min-h-96 px-4 space-y-4 py-4">
-        <h2 className="text-xl md:text-2xl">Choose That Fits Your Needs</h2>
+      <div className="w-full min-h-96 px-4 space-y-4 py-4">
+        <h2 className="text-xl md:text-2xl font-semibold">Pilih Paket yang Sesuai dengan Kebutuhanmu</h2>
         <p className="text-black/70">
-          Our pricing plans are designed to provide the flexibility and features
-          you need to create and share the perfect CV. Compare features and
-          choose the best option for your career.
+          Paket harga kami dirancang untuk memberikan fleksibilitas dan fitur yang kamu butuhkan
+          untuk membuat dan membagikan CV yang sempurna. Bandingkan fitur dan pilih opsi terbaik
+          untuk karirmu.
         </p>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 place-items-center items-start gap-4">
           <PricingCard
-            title="Free"
-            description="Get started with essential feature of creating and sharing your CV."
+            title="Gratis"
+            description="Mulai dengan fitur dasar untuk membuat dan membagikan CV-mu."
             price="Rp 0"
             pricePeriod="/bulan"
-            features={["Online CV", "Basic Template"]}
+            features={["CV Online", "Template Dasar"]}
             onActionClick={() => signIn()}
-            actionLabel="Sign-up"
+            actionLabel="Daftar Sekarang"
           />
           <PricingCard 
             title="Pro"
-            description="Upgrade to Pro"
+            description="Upgrade ke Pro untuk lebih banyak fitur eksklusif."
             price="Rp 19.999"
             pricePeriod="/bulan"
-            features={["Premium Template", "Exclusive URL"]}
+            features={["Template Premium", "URL Eksklusif"]}
             onActionClick={() => signIn()}
-            actionLabel="Sign-up"
+            actionLabel="Daftar Sekarang"
           />
         </div>
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,73 +23,94 @@ const Navbar = () => {
       ? "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground"
       : "";
 
-  const pagesWithAuthenticatedMenu = ["/profile", "/profile/analytics", "/profile/award", "/profile/certification", "/profile/project", "/profile/work-experience", "/profile/billing", "/profile/settings"];
+  const pagesWithAuthenticatedMenu = [
+    "/profile",
+    "/profile/analytics",
+    "/profile/award",
+    "/profile/certification",
+    "/profile/project",
+    "/profile/work-experience",
+    "/profile/billing",
+    "/profile/settings",
+  ];
 
-  const isAuthenticatedMenu = pagesWithAuthenticatedMenu.includes(pathname ?? "");
+  const isAuthenticatedMenu = pagesWithAuthenticatedMenu.includes(
+    pathname ?? ""
+  );
 
   const authenticated = (
-    <div className="flex gap-2">
+    <ul className="flex gap-2 flex-wrap md:flex-nowrap justify-center">
       <Link href="/profile">
         <Button variant="ghost" className={isActive("/profile")}>
           <User />
-          Profile
+          <p className="hidden md:inline-block">Profile</p>
         </Button>
       </Link>
       <Link href="/profile/analytics">
         <Button variant="ghost" className={isActive("/profile/analytics")}>
           <BarChart />
-          Analytics
+          <p className="hidden md:inline-block">Analytics</p>
         </Button>
       </Link>
       <Link href="/profile/billing">
         <Button variant="ghost" className={isActive("/profile/billing")}>
           <CreditCard />
-          Billing
+          <p className="hidden md:inline-block">Billing</p>
         </Button>
       </Link>
       <Link href="/profile/settings">
         <Button variant="ghost" className={isActive("/profile/settings")}>
           <Settings />
-          Settings
+          <p className="hidden md:inline-block">Settings</p>
         </Button>
       </Link>
-    </div>
+    </ul>
   );
 
   const unauthenticated = (
-    <div className="flex gap-2">
+    <ul className="flex gap-2 flex-wrap md:flex-nowrap justify-center">
       <Link href="/">
         <Button variant="ghost" className={isActive("/")}>
           <Home />
-          Home
+          <p className="hidden md:inline-block">Home</p>
         </Button>
       </Link>
       <Link href="/explore">
         <Button variant="ghost" className={isActive("/explore")}>
           <Compass />
-          Explore
+          <p className="hidden md:inline-block">Explore</p>
         </Button>
       </Link>
       <Link href="/pricing">
         <Button variant="ghost" className={isActive("/pricing")}>
           <DollarSign />
-          Pricing
+          <p className="hidden md:inline-block">Pricing</p>
         </Button>
       </Link>
       <Link href="/help">
         <Button variant="ghost" className={isActive("/help")}>
           <LifeBuoy />
-          Help
+          <p className="hidden md:inline-block">Help</p>
         </Button>
       </Link>
-    </div>
+    </ul>
   );
 
   return (
     <header className="w-full flex flex-row justify-between p-4 border-b sticky bg-white z-10 top-0">
-      <div>{isAuthenticatedMenu ? authenticated : unauthenticated}</div>
-      <ul className="flex flex-row justify-center items-center gap-2">
-        {isAuthenticatedMenu ? <LoginButton /> : <Link href='/profile'><Button><User />Profile</Button></Link>}
+      <div className="flex items-center gap-2">
+        {isAuthenticatedMenu ? authenticated : unauthenticated}
+      </div>
+      <ul className="flex items-center gap-2">
+        {isAuthenticatedMenu ? (
+          <LoginButton />
+        ) : (
+          <Link href="/profile">
+            <Button>
+              <User />
+            </Button>
+          </Link>
+        )}
       </ul>
     </header>
   );
