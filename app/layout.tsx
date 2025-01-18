322cc2b99377cec8d579d95a6b0a72ec4a6696c8
dimasgusti <dimsgusticode@gmail.com>
2025-01-18 17:47:07 +0800

    test: layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,32 +3,31 @@
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
 import "./globals.css";
-// import { Toaster } from "sonner";
-// import { Manrope } from "next/font/google";
-// import Navbar from "@/components/Navbar";
+import { Toaster } from "sonner";
+import { Manrope } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
-// const manrope = Manrope({
-//   weight: "400",
-//   subsets: ["latin"],
-//   display: "swap",
-// });
+const manrope = Manrope({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
   session,
 }: {
-  children: React.ReactNode,
-  session: Session | null,
-}) { 
+  children?: React.ReactNode;
+  session: Session | null;
+}) {
   return (
     <html lang="en">
-      {/* <body className={manrope.className}> */}
-      <body>
+      <body className={manrope.className}>
         <SessionProvider session={session}>
           <main className="text-sm antialiased flex flex-col min-h-screen w-full bg-white">
-            {/* <Navbar /> */}
+            <Navbar />
             {children}
-            {/* <Toaster /> */}
+            <Toaster />
           </main>
         </SessionProvider>
       </body>
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@
 export default function Custom404() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl md:text-4xl font-semibold">
+      <h1 className="text-xl md:text-2xl font-semibold">
         404 - Page or User not found.
       </h1>
     </div>
