7ff3da57463858756b524ab6b216a85077e4af43
dimasgusti <dimsgusticode@gmail.com>
2025-01-20 20:13:58 +0800

    test: back to image

--- pages/api/records/index.ts
+++ pages/api/records/index.ts
@@ -54,7 +54,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     presentedBy,
                     url,
                     description,
-                    images: imageUrls, // Save the URLs in the database
+                    image: imageUrls, 
                 }
             });
 
