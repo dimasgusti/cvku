8470c244ecbfa54fea326580cdc29ddfc1f32e94
dimasgusti <dimsgusticode@gmail.com>
2025-01-21 05:47:03 +0800

    bug: removing cretedAt model

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            'avatars.githubusercontent.com',
-            'lh3.googleusercontent.com',
-            'firebasestorage.googleapis.com'
-        ]
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars.githubusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+            },
+        ],
     },
 }
 
