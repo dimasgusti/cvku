180fb5aa9b8db13669286821dae363fb8829b6e2
dimasgusti <dimsgusticode@gmail.com>
2025-01-25 19:44:49 +0800

    feat: userProfile layout

---
+++ app/[username]/layout.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+export default function UsernameLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <header></header>
+      <main className="flex-grow">{children}</main>
+      <footer className="flex flex-col justify-center items-center py-2">
+        <h2 className="text-base md:text-lg">
+          Created with{" "}
+          <Link href="/" className="underline">
+            CVKU
+          </Link>
+        </h2>
+      </footer>
+    </div>
+  );
+}
