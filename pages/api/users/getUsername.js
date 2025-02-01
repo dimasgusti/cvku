43f99d1868d9c7ff543690f39b5830d81c486049
dimasgusti <dimsgusticode@gmail.com>
2025-02-01 19:23:18 +0800

    feat: viewCount increment

--- pages/api/users/getUsername.js
+++ pages/api/users/getUsername.js
@@ -18,6 +18,11 @@ export default async function handler(req, res) {
                 return res.status(404).json({ error: "User not found." });
             }
 
+            await collection.updateOne(
+                { username },
+                { $inc: { viewCount: 1 } }
+            )
+
             res.status(200).json(user);
         } catch (error) {
             console.error(error);
