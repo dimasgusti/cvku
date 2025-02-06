09cefefc65a11da03892b24db46e57d2fe1e6a66
dimasgusti <dimsgusticode@gmail.com>
2025-02-06 13:51:06 +0800

    test: raw data inserted

--- pages/api/webhook/payment.js
+++ pages/api/webhook/payment.js
@@ -3,31 +3,15 @@ import clientPromise from '../../../lib/mongodb';
 
 export async function POST(req) {
   try {
-    const body = await req.json();
-    const { event, data } = body;
+    const { body } = req
 
     const client = await clientPromise;
     const db = client.db('cvku');
     const collection = db.collection('transaction');  
 
-    if (event === 'payment.received' && data?.createdAt) {
-      const createdAtDate = new Date(data.createdAt);
-      const endDate = new Date(createdAtDate.setFullYear(createdAtDate.getFullYear() + 1));
-      
-      await collection.insertOne({
-        userId: data.merchantId,
-        transactionId: data.id,
-        amount: data.amount,
-        createdAt: createdAtDate,
-        endDate: endDate,
-        status: data.status
-      });
-      
-      return NextResponse.json({ success: true, endDate });
-    } else {
-      await collection.insertOne({ event, data });
-      return NextResponse.json({ success: true, message: 'Event logged' });
-    }
+    const result = await collection.insertOne(body);
+
+    return res.status(200).json({ message: 'Data saved successfully', result });
   } catch (error) {
     console.error('Error processing webhook:', error);
     return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
